72296622384a839691413b7e6d586974f1645cef
yamada285 <lindamasahiro@gmail.com>
2020-07-16 21:24:19 +0900

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'static_pages#home'
   get 'static_pages/home'
   get "help" => 'static_pages#help'
   get "about" => 'static_pages#about'
   get "index" => 'maps#index'
   resources :maps, only: [:index, :create]
+  get "/signup" => 'users#new'
+  post "/signup" => 'users#create'
+  resources :users
 end
 
