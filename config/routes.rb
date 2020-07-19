0c331d36e41216f4df487eea803965a4ceda20bf
yamada285 <lindamasahiro@gmail.com>
2020-07-19 14:48:56 +0900

    Finish user edit, update, index, and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get '/signup' => 'users#new'
-  # post '/signup' => 'users#create'
+  post '/signup' => 'users#create'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
