90650c8727cd010aab593f4559b62fe1288f2749
yamada285 <lindamasahiro@gmail.com>
2020-08-03 13:53:52 +0900

    user-name

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ApplicationRecord
-  # belongs_to :user
+  belongs_to :user
   # default_scope -> { order(created_at: :desc) }
   # validates :user_id, presence: true
   # validates :content, presence: true, length: { maximum: 140 }
   mount_uploader :img, ImgUploader
+
+  def user
+    return User.find_by(id: self.user_id)
+  end
 end
